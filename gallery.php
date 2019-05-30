826666778cbf20e6b6cff7abcebeca544aba9573
shpendjahiri <shpe.ndi_3j@hotmail.com>
2019-05-31 01:56:53 +0200

    .

--- gallery.php
+++ gallery.php
@@ -451,6 +451,7 @@ function getVote(int) {
 </script>
 <!-- Footer -->
 
+    
     <footer class="page-width">
     <a href="#top"><img src="images/gotop.png" alt="Gotop" id="gotop""></a>
     <hr class="footer-hr">
@@ -458,7 +459,7 @@ function getVote(int) {
       <div class="footer-div">
         <p class="f-title">COMPANY DETAILS</p>
         <address>
-        <p class="f-text">Euonymus <br>
+        <p class="f-text">TechShop <br>
            13th Avenue 16 <br>
            Brooklyn<br>
            NewYork<br>
@@ -473,32 +474,37 @@ function getVote(int) {
       </div>
       <div class="footer-div">
         <p class="f-title">Top Tech</p>
+        
         <div class="f-lb">
           <img src="images/xs.jpg" alt="tt" width="48px" height="48px" class="f-left">
           <p class="f-text"> Iphone XS - Welcome to the big screens<br>
             <small>Friday , 6th April 2018</small>
           </p>
         </div>
+        
         <div class="f-lb">
           <img src="images/13.jpg" alt="tt" width="48px" height="48px" class="f-left">
           <p class="f-text">  Dell XPS - Designed to be the best<br>
             <small>Friday , 6th April 2018</small>
           </p>
         </div>
+        
         <div class="f-lb">
           <img src="images/15.png" alt="tt" width="48px" height="48px" class="f-left">
           <p class="f-text">  Apple Watch - Fundamentally redesigned and re-engineered <br>
             <small>Friday , 6th April 2018</small>
           </p>
         </div>
+        
         <hr class="footer-hr">
       </div>
-      <div class="footer-div">
+      
+      <div class="footer-div" style="margin-left: 30px">
         <p class="f-title">QUICK LINKS</p>
         <ol type="&raquo;">
-        <a href="trendingnews.html">&raquo;   Trending News</a><br>
-        <a href="iPhonexs.html">&raquo;   Iphone XS</a><br>
-        <a href="gallery.html">&raquo;   Gallery</a>
+
+        <a href="iPhonexs.php"><p style="margin-top: 10px">&raquo;   Iphone XS</a><br>
+        <a href="gallery.php"><p style="margin-top: 10px">&raquo;   Gallery</p></a>
         </ol>
       </div>
       <div class="footer-div">
@@ -507,7 +513,7 @@ function getVote(int) {
       </div> 
     </div>
     <hr class="footer-hr">
-    <p id="f-cr"><em>Copyright &copy; 2019 Euonymus - All rights reserved</em></p>
+    <p id="f-cr"><em>Copyright &copy; 2019 TechShop - All rights reserved</em></p>
   </footer>
   
 
