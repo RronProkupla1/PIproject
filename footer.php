ac0eae446d3861f193701fbfcae60e943778020c
shpendjahiri <shpe.ndi_3j@hotmail.com>
2019-06-01 02:02:45 +0200

    commit

---
+++ footer.php
@@ -0,0 +1,63 @@
+
+    <footer class="page-width">
+    <a href="#top"><img src="images/gotop.png" alt="Gotop" id="gotop""></a>
+    <hr class="footer-hr">
+    <div id="footerd" style="padding: 2%">
+      <div class="footer-div" style="margin-left:8%">
+        <p class="f-title">COMPANY DETAILS</p>
+        <address>
+        <p class="f-text">TechShop <br>
+           13th Avenue 16 <br>
+           Brooklyn<br>
+           NewYork<br>
+           1001
+        </p>
+        </address>
+        <p class="f-text">
+          Tel:0000 000001 <br>
+          Fax:0000 000002 <br>
+          Email:euonymus@euonymus.com
+        </p>
+      </div>
+      <div class="footer-div" style="margin-left: 18%">
+        <p class="f-title">Top Tech</p>
+        <div class="f-lb" style="width: 400px">
+          <img src="images/xs.jpg" alt="tt" width="48px" height="48px" class="f-left">
+          <p class="f-text"> Iphone XS - Welcome to the big screens<br>
+            <small>Friday , 6th April 2018</small>
+          </p>
+        </div>
+        <div class="f-lb" >
+          <img src="images/13.jpg" alt="tt" width="48px" height="48px" class="f-left">
+          <p class="f-text">  Dell XPS - Designed to be the best<br>
+            <small>Friday , 6th April 2018</small>
+          </p>
+        </div>
+        <div class="f-lb">
+          <img src="images/15.png" alt="tt" width="48px" height="48px" class="f-left">
+          <p class="f-text">  Apple Watch - Fundamentally redesigned and re-engineered <br>
+            <small>Friday , 6th April 2018</small>
+          </p>
+        </div>
+        <hr class="footer-hr">
+      </div>
+      <div class="footer-div" style="margin-left: 18%">
+        <p class="f-title">QUICK LINKS</p>
+        <ol type="&raquo;">
+
+        <a href="iPhonexs.php"><p style="margin-top: 10px">&raquo;   Iphone XS</a><br>
+        <a href="gallery.php"><p style="margin-top: 10px">&raquo;   Gallery</p></a>
+        <a href="game.php"><p style="margin-top: 10px">&raquo;   Game</p></a>
+        
+        </ol>
+      </div>
+      <div class="footer-div">
+        
+        </form>
+      </div> 
+    </div>
+    <hr class="footer-hr">
+    <p id="f-cr"><em>Copyright &copy; 2019 TechShop - All rights reserved</em></p>
+  </footer>
+	
+
